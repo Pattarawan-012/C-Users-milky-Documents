fd4283c65eefd24417e3390d38adb5bae9674146
Pattarawan-012 <pattarawan2001@gmail.com>
2021-12-16 11:54:49 +0700

    Final ข้อ 9

---
+++ Final ข้อ 9.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Final2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Write("Input skill: ");
+            string name = Console.ReadLine();
+            while (name != "?")
+            {
+                Node node1 = new Node(name);
+                Console.Write("Add dependency for {0} ? (Y/N) : ", name);
+                char Yes_No = char.Parse(Console.ReadLine());
+                if (Yes_No == 'Y' || Yes_No == 'y')
+                {
+                    Console.Write("Input skill: ");
+                    name = Console.ReadLine();
+                    node1.nextNode = node1;
+                }
+                else if (Yes_No == 'N' || Yes_No == 'n')
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+        class Node
+        {
+            public string name;
+            public Node nextNode;
+
+            public Node(string nameValue)
+            {
+
+                name = nameValue;
+                nextNode = null;
+
+
+            }
+        }
+    }
+}
